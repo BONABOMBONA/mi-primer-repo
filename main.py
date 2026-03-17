18ef15893c8bffab3307f2e10cff3ce1459b39c9
Dana <150980230+BONABOMBONA@users.noreply.github.com>
2026-03-16 23:29:24 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,47 @@
-def saludar(nombre):
-	return f"Hola,{nombre}! Bienvenido a Git"
+import sys
 
-if __name__=="__main__":
-	nombre=input("¿Còmo te llamas?")
-	print(saludar(nombre))
+def limpiar_valor(valor):
+
+    # Ignorar Espacios
+    valor = valor.strip()
+    
+    # Limpiar Caracteres Inválidos
+    caracteres_validos = '0123456789.-'
+    limpio = ''
+    for char in valor:
+        if char in caracteres_validos:
+            limpio += char
+            
+    # Si después de limpiar no queda nada es 0
+    if not limpio:
+        return 0
+        
+    try:
+        return int(float(limpio))
+    except ValueError:
+        # Captura errores
+        return 0
+
+def procesar_linea(linea):
+
+    # Líneas Vacías
+    linea = linea.strip()
+    if not linea:
+        return 0
+        
+    # Múltiples Elementos 
+    valores = linea.split(',')
+    suma = 0
+    
+    for v in valores:
+        suma += limpiar_valor(v)
+        
+    return suma
+
+def main():
+    for linea in sys.stdin:
+        resultado = procesar_linea(linea)
+        print(resultado)
+
+if __name__ == "__main__":
+    main()
